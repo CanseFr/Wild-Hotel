7a4366ba20112bf30225f088e5d2cdb6b7c9b398
CanseFr <jucansell@gmail.com>
2024-12-04 12:18:59 +0100

    :lipstick: Upgrade layout and discover som tailwind class

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,11 +18,12 @@ export const metadata = {
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
-        <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
+        <body className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col `}>
         <Header/>
+        <div className="flex-1 px-8 py-12">
 
-        <main>{children}</main>
-        <footer>Copyright 2024</footer>
+            <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
         </body>
         </html>
     )
