4c1a58b1b033751a8ab0ae63c00b195542f9df54
CanseFr <jucansell@gmail.com>
2024-12-04 11:23:31 +0100

    :lipstick: Diqcover tailwind and apply default tailwind style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import Navigation from "@/app/_components/Navigation";
 import Logo from "@/app/_components/Logo";
 
+import "@/app/_styles/globals.css"
+
 export const metadata = {
     title: "The wild hotel",
 }
