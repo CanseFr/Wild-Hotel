3d1baea73107545b3db4a047c56c67c977aa0385
CanseFr <jucansell@gmail.com>
2024-12-04 11:27:08 +0100

    :lipstick: Discover some param classname from tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
-        <body>
+        <body className="bg-blue-900 text-gray-50 min-h-screen">
         <header>
             <Logo/>
             <Navigation/>
