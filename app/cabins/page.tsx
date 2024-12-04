5bb16ed01cc849b4b22e8ce70fafae86a6d820fe
CanseFr <jucansell@gmail.com>
2024-12-04 10:16:42 +0100

    :memo: Make difference between server component and react server component

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -6,6 +6,9 @@ export default async function Page() {
     const data = await res.json()
 
 
+    console.log("Console log dan sle serveur ")
+
+
     return <div>
         <h1>Cabins page</h1>
         <ul>{data.map((user: any) => (
@@ -15,6 +18,11 @@ export default async function Page() {
                 </ul>
             </li>
         ))}</ul>
-        <Counter/>
+        <Counter user={data}/>
     </div>
 }
+
+
+
+// Ici nous executon une action sur le serveur, donc la page client possedera la data avant de l'envoyer sur le client, on peu le constater avec le console.log, par contre nous appelon un composant enfant Counter
+// QUi lui sera a react server component et s'executera sur le navigateur du client grace a "use client"
